b3d8ca8112815731733918c4cf2bf3f35dc28746
Nancat74 <nastyav74@mail.ru>
2022-08-15 09:55:21 +0500

    feat: guessing-game ended

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mathGuess = Math.round((this.min + this.max) / 2);
+        return this.mathGuess;
     }
 
     lower() {
-
+        this.max = this.mathGuess;
     }
 
     greater() {
-
+        this.min = this.mathGuess;
     }
 }
 
